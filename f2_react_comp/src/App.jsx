45b14250f6057f0d4d0919854f9dc1a4ffdb100d
vineet2812 <vineet_pashine@persistent.com>
2024-02-09 15:16:18 +0530

    components

---
+++ f2_react_comp/src/App.jsx
@@ -0,0 +1,11 @@
+import Hello from './Hello';
+import Random from './Random';
+import VpButton1 from './VpButton';
+import VpButton from './VpButton'
+
+function App(){
+  return <div><h1>Hello</h1><VpButton></VpButton><VpButton1></VpButton1>
+  <Hello></Hello><Random></Random></div>
+}
+
+export default App;
